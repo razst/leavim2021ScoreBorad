2fa721c5de106c2775f98c57843b455a8fef3d35
yego163 <55788331+yego163@users.noreply.github.com>
2021-05-18 00:02:26 +0300

    Add timer

--- score/ScoreUpdate.Designer.cs
+++ score/ScoreUpdate.Designer.cs
@@ -29,10 +29,12 @@ namespace score
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScoreUpdate));
             this.ScorePictureBox = new System.Windows.Forms.PictureBox();
             this.StartButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.TimerVideo = new AxWMPLib.AxWindowsMediaPlayer();
             ((System.ComponentModel.ISupportInitialize)(this.ScorePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TimerVideo)).BeginInit();
             this.SuspendLayout();
             // 
             // ScorePictureBox
@@ -47,7 +49,7 @@ namespace score
             // 
             // StartButton
             // 
-            this.StartButton.Location = new System.Drawing.Point(260, 34);
+            this.StartButton.Location = new System.Drawing.Point(894, 384);
             this.StartButton.Name = "StartButton";
             this.StartButton.Size = new System.Drawing.Size(131, 55);
             this.StartButton.TabIndex = 1;
@@ -55,30 +57,30 @@ namespace score
             this.StartButton.UseVisualStyleBackColor = true;
             this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
             // 
-            // label1
+            // TimerVideo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(391, 380);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.TimerVideo.Enabled = true;
+            this.TimerVideo.Location = new System.Drawing.Point(1489, 809);
+            this.TimerVideo.Name = "TimerVideo";
+            this.TimerVideo.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("TimerVideo.OcxState")));
+            this.TimerVideo.Size = new System.Drawing.Size(366, 206);
+            this.TimerVideo.TabIndex = 3;
             // 
             // ScoreUpdate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::score.Properties.Resources.ניקוד_ירוחם;
-            this.ClientSize = new System.Drawing.Size(1604, 881);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.TimerVideo);
             this.Controls.Add(this.StartButton);
             this.Controls.Add(this.ScorePictureBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "ScoreUpdate";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.ScorePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TimerVideo)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -86,6 +88,6 @@ namespace score
 
         private System.Windows.Forms.PictureBox ScorePictureBox;
         private System.Windows.Forms.Button StartButton;
-        private System.Windows.Forms.Label label1;
+        private AxWMPLib.AxWindowsMediaPlayer TimerVideo;
     }
 }
